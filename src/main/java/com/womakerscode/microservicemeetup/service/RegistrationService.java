0e1cf5e9d0e51f793eb84a14c012a985d0d16179
brunaguimaraesb <brunaguimaraes.santos@hotmail.com>
2022-05-05 19:45:35 -0300

    projeto final

---
+++ src/main/java/com/womakerscode/microservicemeetup/service/RegistrationService.java
@@ -0,0 +1,22 @@
+package com.womakerscode.microservicemeetup.service;
+
+import com.womakerscode.microservicemeetup.model.entity.Registration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+    public interface RegistrationService {
+
+        Registration save(Registration any);
+
+        Optional<Registration> getRegistrationById(Integer id);
+
+        void delete(Registration registration);
+
+        Registration update(Registration registration);
+
+        Page<Registration> find(Registration filter, Pageable pageRequest);
+
+        Optional<Registration> getRegistrationByRegistrationAttribute(String registrationAttribute);
+}
